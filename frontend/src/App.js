ef250d28c021ecdd5534bb18b72c2a646f2d9082
Vignesh-King <vigneshveera2108@gmail.com>
2025-01-19 12:37:15 +0530

    Adding Hi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Team Edit TEST <code>src/App.js</code> and save
+          Hello Team Edit TEST (Hi) 
+          
+          <code>src/App.js</code> and save
           to reload.
         </p>
         <a
