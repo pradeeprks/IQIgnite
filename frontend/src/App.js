e33f23b0bd45e3d7fd5654c9c74cc1fd47570b66
pradeep <pradeeprks27@gmail.com>
2025-01-19 12:55:57 +0530

    folder created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Team Edit TEST <code>src/App.js</code> and save
-          to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Function to check if the user is authenticated
+
+const App = () => {
+    
+
+    return (
+        <p>Test</p> 
+    );
+};
 
 export default App;
